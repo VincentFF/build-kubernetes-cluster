d8671db83b28e66c3ba7935e4ce1ce08ed9e3aa5
Vincent <fanvincent@hotmail.com>
2018-08-21 20:42:59 +0700

    用自己生成签证的方式替换kubeadm自动生成签证，签证有效期从一年延长为十年

--- roles/etcd/scripts/gencerts.sh
+++ roles/etcd/scripts/gencerts.sh
@@ -3,12 +3,33 @@
 dir=$(cd `dirname $0` && pwd)
 files="${dir}/../files"
 cd $dir
-# gen certs
-./cfssl gencert -initca ${files}/ca-csr.json | ./cfssljson -bare ${files}/ca
-./cfssl gencert -ca=${files}/ca.pem -ca-key=${files}/ca-key.pem -config=${files}/ca-config.json -profile=client ${files}/client.json | ./cfssljson -bare ${files}/client
-./cfssl gencert -ca=${files}/ca.pem -ca-key=${files}/ca-key.pem -config=${files}/ca-config.json -profile=server ${files}/server.json| ./cfssljson -bare ${files}/server
-./cfssl gencert -ca=${files}/ca.pem -ca-key=${files}/ca-key.pem -config=${files}/ca-config.json -profile=peer ${files}/peer.json| ./cfssljson -bare ${files}/peer
 
-cp ${files}/ca.pem ${dir}/../../masters/files/etcd/ca.crt
-cp ${files}/client.pem ${dir}/../../masters/files/apiserver-etcd-client.crt
-cp ${files}/client-key.pem ${dir}/../../masters/files/apiserver-etcd-client.key
+# generate etcd certs
+./cfssl gencert -initca ${files}/etcd/ca-csr.json | ./cfssljson -bare ${files}/etcd/ca
+./cfssl gencert -ca=${files}/etcd/ca.pem -ca-key=${files}/etcd/ca-key.pem -config=${files}/etcd/ca-config.json -profile=client ${files}/etcd/client.json | ./cfssljson -bare ${files}/etcd/client
+./cfssl gencert -ca=${files}/etcd/ca.pem -ca-key=${files}/etcd/ca-key.pem -config=${files}/etcd/ca-config.json -profile=server ${files}/etcd/server.json| ./cfssljson -bare ${files}/etcd/server
+./cfssl gencert -ca=${files}/etcd/ca.pem -ca-key=${files}/etcd/ca-key.pem -config=${files}/etcd/ca-config.json -profile=peer ${files}/etcd/peer.json| ./cfssljson -bare ${files}/etcd/peer
+
+# generate kubernetes certs
+./cfssl gencert -initca ${files}/kubernetes/ca-csr.json | ./cfssljson -bare ${files}/kubernetes/ca
+./cfssl gencert -ca=${files}/kubernetes/ca.pem -ca-key=${files}/kubernetes/ca-key.pem -config=${files}/kubernetes/ca-config.json -profile=kubernetes ${files}/kubernetes/apiserver.json| ./cfssljson -bare ${files}/kubernetes/apiserver
+./cfssl gencert -ca=${files}/kubernetes/ca.pem -ca-key=${files}/kubernetes/ca-key.pem -config=${files}/kubernetes/ca-config.json -profile=kubernetes ${files}/kubernetes/apiserver-kubelet-client.json| ./cfssljson -bare ${files}/kubernetes/apiserver-kubelet-client
+./cfssl gencert -ca=${files}/kubernetes/ca.pem -ca-key=${files}/kubernetes/ca-key.pem -config=${files}/kubernetes/ca-config.json -profile=kubernetes ${files}/kubernetes/front-proxy-client.json| ./cfssljson -bare ${files}/kubernetes/front-proxy-client
+
+
+# copy etcd certs to kubernetes
+cp ${files}/etcd/ca.pem ${dir}/../../masters/files/etcd/ca.crt
+cp ${files}/etcd/client.pem ${dir}/../../masters/files/apiserver-etcd-client.crt
+cp ${files}/etcd/client-key.pem ${dir}/../../masters/files/apiserver-etcd-client.key
+
+# copy kubernetes certs
+cp ${files}/kubernetes/ca.pem ${dir}/../../masters/files/ca.crt
+cp ${files}/kubernetes/ca-key.pem ${dir}/../../masters/files/ca.key
+cp ${files}/kubernetes/ca.pem ${dir}/../../masters/files/front-proxy-ca.crt
+cp ${files}/kubernetes/ca-key.pem ${dir}/../../masters/files/front-proxy-ca.key
+cp ${files}/kubernetes/apiserver.pem ${dir}/../../masters/files/apiserver.crt
+cp ${files}/kubernetes/apiserver-key.pem ${dir}/../../masters/files/apiserver.key
+cp ${files}/kubernetes/apiserver-kubelet-client.pem ${dir}/../../masters/files/apiserver-kubelet-client.crt
+cp ${files}/kubernetes/apiserver-kubelet-client-key.pem ${dir}/../../masters/files/apiserver-kubelet-client.key
+cp ${files}/kubernetes/front-proxy-client.pem ${dir}/../../masters/files/front-proxy-client.crt
+cp ${files}/kubernetes/front-proxy-client-key.pem ${dir}/../../masters/files/front-proxy-client.key
