e68b07937bce38c77ffa18209b727251ef8301b1
doors <doors@email.com>
2018-09-28 17:29:03 +0800

    fix bugs

--- delete.sh
+++ delete.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 ansible -i hosts all -a "kubeadm reset -f"
-ansible -i hosts masters -a "systemctl stop etcd"
-ansible -i hosts masters -a "rm -rf /etc/ssl/etcd/*"
-ansible -i hosts masters -a "rm -rf /var/lib/etcd/default.etcd/*"
+ansible -i hosts etcds -a "systemctl stop etcd"
+ansible -i hosts etcds -a "rm -rf /etc/ssl/etcd/*"
+ansible -i hosts etcds -a "rm -rf /var/lib/etcd/default.etcd/*"
